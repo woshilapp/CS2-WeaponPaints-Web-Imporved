4adfbe5966d4c559034516081e09591494ed6cc3
woshilapp <122607391+woshilapp@users.noreply.github.com>
2025-01-18 23:07:13 +0800

    My fking steam api key is uploaded

--- class/config.php
+++ class/config.php
@@ -5,7 +5,7 @@ define('DB_NAME', 'skin');
 define('DB_USER', 'skin');
 define('DB_PASS', 'skin');
 
-define('STEAM_API_KEY', '9EDBFE823501EA3AF65EB9BEC3F52D5D');
+define('STEAM_API_KEY', '');
 define('STEAM_DOMAIN_NAME', '');
 define('STEAM_LOGOUT_PAGE', '');
 define('STEAM_LOGIN_PAGE', '');
